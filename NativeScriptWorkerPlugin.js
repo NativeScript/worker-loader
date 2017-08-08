323920ed6ee170aeb22ef254c39b32abdd090cbd
sis0k0 <stanimira.vlaeva@gmail.com>
2017-08-08 20:18:44 +0300

    refactor: create workers-chunks.json file for emitted workers

---
+++ NativeScriptWorkerPlugin.js
@@ -0,0 +1,25 @@
+const { resolve } = require("path");
+const { RawSource } = require("webpack-sources");
+
+exports.NativeScriptWorkerPlugin = (function() {
+    function NativeScriptWorkerPlugin() {
+    }
+
+    NativeScriptWorkerPlugin.prototype.apply = function(compiler) {
+        compiler.plugin("emit", (compilation, cb) => {
+            const output = compiler.outputPath;
+            const workersFullPath = compilation.workerChunks
+                .map(chunk => resolve(output, chunk));
+
+            const content = JSON.stringify(workersFullPath, null, 4);
+            const source = new RawSource(content);
+
+            compilation.assets["__worker-chunks.json"] = source;
+
+            cb();
+        });
+    };
+
+    return NativeScriptWorkerPlugin;
+})();
+
