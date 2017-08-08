4f9e6eb5bcc4f893fcaa4edbd2d2154294b1d2dc
sis0k0 <stanimira.vlaeva@gmail.com>
2017-08-08 17:07:59 +0300

    refactor: remove inline workers and no fallback option

--- index.js
+++ index.js
@@ -1,77 +1,101 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const loaderUtils = require('loader-utils');
-const validateOptions = require('schema-utils');
-const WebWorkerTemplatePlugin = require('webpack/lib/webworker/WebWorkerTemplatePlugin');
-const NodeTargetPlugin = require('webpack/lib/node/NodeTargetPlugin');
-const SingleEntryPlugin = require('webpack/lib/SingleEntryPlugin');
-const schema = require('./options.json');
+const path = require("path");
+const loaderUtils = require("loader-utils");
+const validateOptions = require("schema-utils");
+const WebWorkerTemplatePlugin = require("webpack/lib/webworker/WebWorkerTemplatePlugin");
+const NodeTargetPlugin = require("webpack/lib/node/NodeTargetPlugin");
+const SingleEntryPlugin = require("webpack/lib/SingleEntryPlugin");
+const schema = require("./options.json");
 
-const getWorker = (file, content, options) => {
-  const root = JSON.stringify('./');
-  const filePath = JSON.stringify(file);
-  const workerPublicPath = `${root} + __webpack_public_path__ + ${filePath}`;
-  if (options.inline) {
-    const createInlineWorkerPath = JSON.stringify(`!!${path.join(__dirname, 'createInlineWorker.js')}`);
-    const fallbackWorkerPath = options.fallback === false ? 'null' : workerPublicPath;
-    return `require(${createInlineWorkerPath})(${JSON.stringify(content)}, ${fallbackWorkerPath})`;
-  }
-  return `new Worker(${workerPublicPath})`;
+const getWorker = (file, _content, options) => {
+    if (options.inline) {
+        throw new Error("The NativeScript worker loader doesn't support inline workers!")
+    }
+
+    if (options.fallback === false) {
+        throw new Error("The NativeScript worker loader " + 
+            "cannot be used without a fallback webworker script!");
+    }
+
+    const workerPublicPath = getPublicPath(file);
+
+    return `new Worker(${workerPublicPath})`;
 };
+
+const getPublicPath = file => {
+    const root = JSON.stringify("./");
+    const filePath = JSON.stringify(file);
+
+    return `${root} + __webpack_public_path__ + ${filePath}`;
+}
 
 module.exports = function workerLoader() {};
 
 module.exports.pitch = function pitch(request) {
-  if (!this.webpack) throw new Error('Only usable with webpack');
-  this.cacheable(false);
-  const callback = this.async();
-  const options = loaderUtils.getOptions(this) || {};
+    if (!this.webpack) {
+        throw new Error("Only usable with webpack");
+    }
 
-  validateOptions(schema, options, 'Worker Loader');
+    this.cacheable(false);
+    const callback = this.async();
+    const options = loaderUtils.getOptions(this) || {};
 
-  const filename = loaderUtils.interpolateName(this, options.name || '[hash].worker.js', {
-    context: options.context || this.options.context,
-    regExp: options.regExp,
-  });
-  const outputOptions = {
-    filename,
-    chunkFilename: `[id].${filename}`,
-    namedChunkFilename: null,
-  };
-  if (this.options && this.options.worker && this.options.worker.output) {
-    Object.keys(this.options.worker.output).forEach((name) => {
-      outputOptions[name] = this.options.worker.output[name];
+    validateOptions(schema, options, "Worker Loader");
+
+    const filename = loaderUtils.interpolateName(this, options.name || "[hash].worker.js", {
+        context: options.context || this.options.context,
+        regExp: options.regExp,
     });
-  }
-  const workerCompiler = this._compilation.createChildCompiler('worker', outputOptions);
-  workerCompiler.apply(new WebWorkerTemplatePlugin(outputOptions));
-  if (this.target !== 'webworker' && this.target !== 'web') {
-    workerCompiler.apply(new NodeTargetPlugin());
-  }
-  workerCompiler.apply(new SingleEntryPlugin(this.context, `!!${request}`, 'main'));
-  if (this.options && this.options.worker && this.options.worker.plugins) {
-    this.options.worker.plugins.forEach(plugin => workerCompiler.apply(plugin));
-  }
-  const subCache = `subcache ${__dirname} ${request}`;
-  workerCompiler.plugin('compilation', (compilation) => {
-    if (compilation.cache) {
-      if (!compilation.cache[subCache]) {
-        compilation.cache[subCache] = {};
-      }
-      compilation.cache = compilation.cache[subCache];
+
+    const outputOptions = {
+        filename,
+        chunkFilename: `[id].${filename}`,
+        namedChunkFilename: null,
+    };
+
+    if (this.options && this.options.worker && this.options.worker.output) {
+        Object.keys(this.options.worker.output).forEach((name) => {
+            outputOptions[name] = this.options.worker.output[name];
+        });
     }
-  });
-  workerCompiler.runAsChild((err, entries, compilation) => {
-    if (err) return callback(err);
-    if (entries[0]) {
-      const workerFile = entries[0].files[0];
-      const workerFactory = getWorker(workerFile, compilation.assets[workerFile].source(), options);
-      if (options.fallback === false) {
-        delete this._compilation.assets[workerFile];
-      }
-      return callback(null, `module.exports = function() {\n\treturn ${workerFactory};\n};`);
+
+    const workerCompiler = this._compilation.createChildCompiler("worker", outputOptions);
+    workerCompiler.apply(new WebWorkerTemplatePlugin(outputOptions));
+    if (this.target !== "webworker" && this.target !== "web") {
+        workerCompiler.apply(new NodeTargetPlugin());
     }
-    return callback(null, null);
-  });
+
+    workerCompiler.apply(new SingleEntryPlugin(this.context, `!!${request}`, "main"));
+    if (this.options && this.options.worker && this.options.worker.plugins) {
+        this.options.worker.plugins.forEach(plugin => workerCompiler.apply(plugin));
+    }
+
+    const subCache = `subcache ${__dirname} ${request}`;
+    workerCompiler.plugin("compilation", compilation => {
+        if (compilation.cache) {
+            if (!compilation.cache[subCache]) {
+                compilation.cache[subCache] = {};
+            }
+            compilation.cache = compilation.cache[subCache];
+        }
+    });
+
+    workerCompiler.runAsChild((err, entries, compilation) => {
+        if (err) {
+            return callback(err);
+        }
+
+        if (entries[0]) {
+            const workerFile = entries[0].files[0];
+            const workerFactory = getWorker(workerFile, compilation.assets[workerFile].source(), options);
+            if (options.fallback === false) {
+                delete this._compilation.assets[workerFile];
+            }
+
+            return callback(null, `module.exports = function() {\n\treturn ${workerFactory};\n};`);
+        }
+
+        return callback(null, null);
+    });
 };
