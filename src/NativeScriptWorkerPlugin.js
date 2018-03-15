21f79458393b9a21ca599b08a3a18d60a00b1d7b
Peter Staev <peter@tangrasoft.com>
2018-03-15 23:39:33 +0200

    improved worker loader implementation

--- src/NativeScriptWorkerPlugin.js
+++ src/NativeScriptWorkerPlugin.js
@@ -1,28 +1,64 @@
-const { resolve } = require("path");
+const { parse, resolve } = require("path");
 const { RawSource } = require("webpack-sources");
 
 exports.NativeScriptWorkerPlugin = (function () {
-    function NativeScriptWorkerPlugin() {
+    function NativeScriptWorkerPlugin({commonChunkName}) {
+        this.files = {};
+        this.commonChunkName = commonChunkName;
     }
 
     NativeScriptWorkerPlugin.prototype.apply = function (compiler) {
-        compiler.plugin("emit", (compilation, cb) => {
-            if (!compilation.workerChunks) {
-                return cb();
-            }
+        compiler.plugin("this-compilation", (compilation) => {
+            
+            compilation.plugin(["optimize-chunks"], (chunks) => {
+                const workersFullPath = [];
 
-            const output = compiler.outputPath;
-            const workersFullPath = compilation.workerChunks
-                .map(chunk => resolve(output, chunk));
+                for (const chunk of chunks) {
+                    if (chunk._isWorkerExtractedIn) {
+                        continue;
+                    }
+                    for (const module of chunk.modulesIterable) {
+                        if (compilation.workerFiles.has(module.request)) {
+                            const fileName = compilation.workerFiles.get(module.request)
+                            const workerChunk = compilation.addChunk(fileName);
+                            
+                            workerChunk.filenameTemplate = fileName
+                            chunk.moveModule(module, workerChunk);
+                            workerChunk.entryModule = module;
+                            workerChunk._isWorkerExtractedIn = true;
 
-            const content = JSON.stringify(workersFullPath, null, 4);
-            const source = new RawSource(content);
+                            this.addAsset(compilation, fileName.replace(".worker.js", ".starter.js"), this.generateStarterModule(fileName))
 
-            compilation.assets["__worker-chunks.json"] = source;
+                            workersFullPath.push(resolve(compiler.outputPath, fileName));
+                            // console.log(join(compiler.outputPath, fileName));
+                            // return;
+                        }
+                    }
+                }
 
-            cb();
+                if (workersFullPath.length === 0) {
+                    return;
+                }
+                
+                const content = JSON.stringify(workersFullPath, null, 4);
+                const source = new RawSource(content);
+    
+                compilation.assets["__worker-chunks.json"] = source;
+            });            
         });
     };
+
+    NativeScriptWorkerPlugin.prototype.generateStarterModule = function (worker) {
+        return `require("./${this.commonChunkName}");
+require("./${parse(worker).name}");`
+    }
+
+    NativeScriptWorkerPlugin.prototype.addAsset = function(compilation, name, content) {
+        if (this.files[name] !== content) {
+            this.files[name] = content;
+            compilation.assets[name] = new RawSource(content);
+        }
+    }
 
     return NativeScriptWorkerPlugin;
 }());
