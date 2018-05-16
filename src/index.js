e227f120da8012cbe2e8b949606ced01acb8bbc0
Peter Staev <PeterStaev@users.noreply.github.com>
2018-05-16 16:40:00 +0300

    Improve loaders to use the vendor chunk and reduce their size (#12)
    * improved worker loader implementation
    
    * add null check
    
    * better adding for worker modules and work with vendor chunk directly
    
    * added default value for easier migration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 "use strict";
 
+const { basename } = require("path");
 const loaderUtils = require("loader-utils");
 const validateOptions = require("schema-utils");
-const WebWorkerTemplatePlugin = require("webpack/lib/webworker/WebWorkerTemplatePlugin");
-const NodeTargetPlugin = require("webpack/lib/node/NodeTargetPlugin");
-const SingleEntryPlugin = require("webpack/lib/SingleEntryPlugin");
 const optionsSchema = require("./options.json");
 
 const validateSchema = (schema, options, pluginName) => {
@@ -32,71 +30,24 @@ const getWorker = file => {
     return `new Worker(${workerPublicPath})`;
 };
 
-module.exports = function workerLoader() {};
+module.exports = function workerLoader(content, map, meta) {
+    this._compilation.workerFiles = this._compilation.workerFiles || new Map();
 
-module.exports.pitch = function pitch(request) {
-    if (!this.webpack) {
-        throw new Error("Only usable with webpack");
-    }
-
-    this.cacheable(false);
-    const callback = this.async();
-    const options = loaderUtils.getOptions(this) || {};
-    validateSchema(optionsSchema, options, "Worker Loader");
-    this._compilation.workerChunks = [];
-
-    const filename = loaderUtils.interpolateName(this, options.name || "[hash].worker.js", {
-        context: options.context || this.options.context,
-        regExp: options.regExp,
+    const filenamePrefix = loaderUtils.interpolateName(this, "[hash]", {
+        context: this.context,
+        content,
     });
 
-    const outputOptions = {
-        filename,
-        chunkFilename: `[id].${filename}`,
-        namedChunkFilename: null,
-    };
+    this._compilation.workerFiles.set(this.data.path, `${filenamePrefix}.worker.js`);
 
-    if (this.options && this.options.worker && this.options.worker.output) {
-        Object.keys(this.options.worker.output).forEach((name) => {
-            outputOptions[name] = this.options.worker.output[name];
-        });
-    }
-
-    const workerCompiler = this._compilation.createChildCompiler("worker", outputOptions);
-    workerCompiler.apply(new WebWorkerTemplatePlugin(outputOptions));
-    if (this.target !== "webworker" && this.target !== "web") {
-        workerCompiler.apply(new NodeTargetPlugin());
-    }
-
-    workerCompiler.apply(new SingleEntryPlugin(this.context, `!!${request}`, "main"));
-    if (this.options && this.options.worker && this.options.worker.plugins) {
-        this.options.worker.plugins.forEach(plugin => workerCompiler.apply(plugin));
-    }
-
-    const subCache = `subcache ${__dirname} ${request}`;
-    workerCompiler.plugin("compilation", compilation => {
-        if (compilation.cache) {
-            if (!compilation.cache[subCache]) {
-                compilation.cache[subCache] = {};
-            }
-            compilation.cache = compilation.cache[subCache];
-        }
-    });
-
-    workerCompiler.runAsChild((err, entries) => {
-        if (err) {
-            return callback(err);
-        }
-
-        if (entries[0]) {
-            const workerFile = entries[0].files[0];
-            this._compilation.workerChunks.push(workerFile);
-            const workerFactory = getWorker(workerFile);
-
-            return callback(null, `module.exports = function() {\n\treturn ${workerFactory};\n};`);
-        }
-
-        return callback(null, null);
-    });
+    // require.context is used only to add the module as dependency so webpack can build it
+    return `
+module.exports = function() {
+    require.context("~/", true, /${basename(this.data.path)}$/);
+    return ${getWorker(filenamePrefix + ".starter.js")};
+};`;
 };
 
+module.exports.pitch = function (request, preceding, data) {
+    data.path = request;
+};
