6f8ad0ec7a1b4eb7bcf6d10244b9bf233da4e12f
Kristian Dimitrov <kddimitrov@progress.com>
2019-11-15 18:13:32 +0200

    fix: disable HMR in child compilation and clear compilation hash

--- src/index.js
+++ src/index.js
@@ -88,6 +88,15 @@ module.exports.pitch = function pitch(request) {
     }
 
     new SingleEntryPlugin(this.context, `!!${request}`, "main").apply(workerCompiler);
+    const plugin = { name: "WorkerLoader" };
+
+    workerCompiler.hooks.thisCompilation.tap(plugin, compilation => {
+        /**
+         * A dirty hack to disable HMR plugin in childCompilation - https://github.com/webpack/webpack/blob/4056506488c1e071dfc9a0127daa61bf531170bf/lib/HotModuleReplacementPlugin.js#L154;
+         * Once we update to webpack@4.40.3 and above this can be removed - https://github.com/webpack/webpack/commit/1c4138d6ac04b7b47daa5ec4475c0ae1b4f596a2
+         */
+        compilation.hotUpdateChunkTemplate = null;
+    });
 
     workerCompiler.runAsChild((err, entries, childCompilation) => {
         if (err) {
@@ -100,6 +109,11 @@ module.exports.pitch = function pitch(request) {
             fileDeps.map(fileName => {
                 this.addDependency(fileName);
             });
+            /**
+             * Clears the hash of the child compilation as it affects the hash of the parent compilation - https://github.com/webpack/webpack/blob/4056506488c1e071dfc9a0127daa61bf531170bf/lib/Compilation.js#L2281
+             * If we don't clear the hash an emit of runtime.js and an empty [somehash].hot-update.json will happen, which will restart the NS application.
+             */
+            childCompilation.hash = "";
             const workerFile = entries[0].files[0];
             this._compilation.workerChunks.push(workerFile);
             const workerFactory = getWorker(workerFile);
