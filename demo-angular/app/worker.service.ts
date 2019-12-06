c7a646e134c439d14c41992219d915c2b606208d
DimitarTachev <nevermindd7@gmail.com>
2019-12-06 15:05:34 +0200

    docs: update the angular demo in order to terminate the workers on LiveSync

--- demo-angular/app/worker.service.ts
+++ demo-angular/app/worker.service.ts
@@ -1,33 +1,43 @@
-import { Injectable } from "@angular/core";
+import { Injectable, OnDestroy } from "@angular/core";
 
 // add if building with webpack
 import * as TsWorker from "nativescript-worker-loader!./workers/typescript.worker";
 
 @Injectable()
-export class WorkerService {
+export class WorkerService implements OnDestroy {
+    jsWorker: null;
+    tsWorker: null;
     constructor() {
     }
 
+    ngOnDestroy() {
+        if (this.jsWorker) {
+            (<any>this.jsWorker).terminate();
+        }
+        if (this.tsWorker) {
+            (<any>this.tsWorker).terminate();
+        }
+    }
+
     initTsWorker() {
-        // add if building with webpack
-        const worker = new TsWorker();
-
-        // remove if building with webpack
-        // const worker = new Worker("./workers/typescript.worker");
-
-        return worker;
-    } 
-
-    initJsWorker() {
-        let worker: Worker;
-
-        if ((<any>global).TNS_WEBPACK) {
-            const JsWorker = require("nativescript-worker-loader!./workers/javascript.worker.js");
-            worker = new JsWorker();
-        } else {
-            worker = new Worker("./workers/javascript.worker.js");
+        if (this.tsWorker) {
+            return this.tsWorker;
         }
 
-        return worker;
+        // add if building with webpack
+        this.tsWorker = new TsWorker();
+
+        return this.tsWorker;
+    }
+
+    initJsWorker() {
+        if (this.jsWorker) {
+            return this.jsWorker;
+        }
+
+        const JsWorker = require("nativescript-worker-loader!./workers/javascript.worker.js");
+        this.jsWorker = new JsWorker();
+
+        return this.jsWorker;
     }
 }
--- demo-angular/app/workers/javascript.worker.js
+++ demo-angular/app/workers/javascript.worker.js
@@ -1,6 +1,6 @@
 require("tns-core-modules/globals");
 
-global.onmessage = function(msg) {
+global.onmessage = function (msg) {
     console.log("Inside JS worker...");
     console.log(msg);
     global.postMessage("JS worker");
